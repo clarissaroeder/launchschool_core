b63d9695b29a96756689eba375c7ee10a40be738
Clarissa Roeder <clari1294@gmail.com>
2023-09-27 11:57:23 +0200

    more ruby easy 1 exercises complete

--- exercises/more_ruby/easy_1/3.rb
+++ exercises/more_ruby/easy_1/3.rb
@@ -1,6 +1,14 @@
+=begin
+Write a method that takes a sorted array of integers as an argument, 
+and returns an array that includes all of the missing integers (in order) 
+between the first and last elements of the argument.
+=end
 
+def missing(array)
+  (array.first..array.last).to_a - array
+end
 
-missing([-3, -2, 1, 5]) == [-1, 0, 2, 3, 4]
-missing([1, 2, 3, 4]) == []
-missing([1, 5]) == [2, 3, 4]
-missing([6]) == []
+p missing([-3, -2, 1, 5]) == [-1, 0, 2, 3, 4]
+p missing([1, 2, 3, 4]) == []
+p missing([1, 5]) == [2, 3, 4]
+p missing([6]) == []
