6c1a795db87dd7f3790be74d99ba69168f72e8e5
Clarissa Roeder <clari1294@gmail.com>
2023-10-03 16:15:39 +0200

    more ruby exercises easy testing complete

---
+++ exercises/more_ruby/easy_testing/1.rb
@@ -0,0 +1,5 @@
+# Write a minitest assertion that will fail if the value.odd? is not true.
+
+assert(value.odd?, "the value is not odd")
+
+# First argument is tested for truthiness, the second is the error method that will be displayed if the first argument is not truthy
