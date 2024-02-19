b375c7dc9e28c80be891429dbb5dc66d5fda8f11
Clarissa Roeder <clari1294@gmail.com>
2024-02-19 10:47:15 +0100

    rb175 review

---
+++ RB175/lesson_3/book_viewer_1/book_viewer.rb
@@ -0,0 +1,37 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join
+  end
+end
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+
+  erb :home
+end
+
+get "/chapters/:number" do
+  number = params[:number].to_i
+
+  redirect "/" unless (1..@contents.size).cover?(number)
+
+  chapter_name = @contents[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
+
+  erb :chapter
+end
+
+not_found do 
+  redirect "/"
+end
